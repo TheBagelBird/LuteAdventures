e29a7c3c156e843126807582edbff3bb136b0ba8
Bagel Bird (Laurie) <morinelias64@gmail.com>
2021-10-19 23:52:54 -0400

    cleaned up the playercontroller

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,42 +10,18 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private LayerMask obstacleMask;
 
-    private PlayerMovement playerControls;
-
-    // Start is called before the first frame update
-    void Start()
+    private void Awake()
     {
         //remove the move point from the player cause I only did that for the sake of organization
         movePoint.parent = null;
     }
 
-    private void Awake()
-    {
-        playerControls = new PlayerMovement();
-    }
-
-    private void OnEnable()
-    {
-        playerControls.Enable();
-    }
-
-    private void OnDisable()
-    {
-        playerControls.Disable();
-    }
-
-    public void Horizontal(InputAction.CallbackContext context)
-    {
-        
-    }
-
-
     // Update is called once per frame
     void Update()
     {
-        transform.position = Vector3.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
+        transform.position = Vector2.MoveTowards(transform.position, movePoint.position, moveSpeed * Time.deltaTime);
 
-        if (Vector3.Distance(transform.position, movePoint.position) <= .5f)
+        if (Vector2.Distance(transform.position, movePoint.position) <= .5f)
         {
 
             if (Mathf.Abs(Input.GetAxisRaw("Horizontal")) == 1)
